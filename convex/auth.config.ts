b2eb95dc8e31cc7a13a14250acf0632afc694c99
DevilCoders <119003992+DevilCoders@users.noreply.github.com>
2025-07-07 22:46:36 +0300

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://real-civet-44.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
